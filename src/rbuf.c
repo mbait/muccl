9ecdfa3b48add85282684a97fa9264577d8f9dc3
Alexander Solovets <asolovets+gpg@gmail.com>
2017-06-24 22:13:45 -0700

    [rbuf] Reduce duplication with __auto_type

--- src/rbuf.c
+++ src/rbuf.c
@@ -19,8 +19,8 @@ rbuf_alloc (size_t size)
 
 #define MIN(a, b)                                                             \
   __extension__({                                                             \
-    __typeof__(a) _a = (a);                                                   \
-    __typeof__(b) _b = (b);                                                   \
+    __auto_type _a = (a);                                                     \
+    __auto_type _b = (b);                                                     \
     _a <= _b ? _a : _b;                                                       \
   })
 
